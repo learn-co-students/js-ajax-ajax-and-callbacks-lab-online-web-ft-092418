42b6775bcb740ff86dae97322d9eefcf48fdebc3
mpissanos <mpissano@gmail.com>
2019-03-04 14:32:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-$(document).ready(function (){
+
+$(document).ready(function () {
 });
+
+function searchRepositories() {
+  let terms = $('#searchTerms')[0].value
+  $.get(`https://api.github.com/search/repositories?q=${terms}`, function (data) {
+    let response = data.items
+    showRepositories(response)
+
+  }).fail(function (error) {
+    displayError()
+  })
+}
+
+function showRepositories(response) {
+  const template = Handlebars.compile(document.getElementById('repository-template').innerHTML);
+  const repoList = template(response);
+
+  document.getElementById('results').innerHTML = repoList
+}
+
+function showCommits(el) {
+  
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function (data) {
+    const template = Handlebars.compile(document.getElementById('commits-template').innerHTML)
+    document.getElementById('details').innerHTML = template(data)
+  }).fail(function (error) {
+    displayError()
+  })
+}
+
+function displayError() {
+  let message = '<p>I\'m sorry, there\'s been an error. Please try again.</p>'
+  document.getElementById('errors').innerHTML = message
+}
